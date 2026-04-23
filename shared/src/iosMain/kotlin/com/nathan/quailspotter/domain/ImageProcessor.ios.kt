6f501be5f3bd63b5eee191168132b5fb6075d1e0
Nathan Mellor <nathan.d.mellor@gmail.com>
2026-04-23 13:38:23 -0700

    Implement iOS image processing for TFLite

--- shared/src/iosMain/kotlin/com/nathan/quailspotter/domain/ImageProcessor.ios.kt
+++ shared/src/iosMain/kotlin/com/nathan/quailspotter/domain/ImageProcessor.ios.kt
@@ -8,20 +8,50 @@ import kotlinx.cinterop.*
 actual class ImageProcessor {
     @OptIn(ExperimentalForeignApi::class)
     actual fun resizeForUint8(imageData: ByteArray, width: Int, height: Int): Any {
-        val uiImage = UIImage(data = imageData.toNSData())
-        val size = CGSizeMake(width.toDouble(), height.toDouble())
+        val nsData = imageData.toNSData()
+        val uiImage = UIImage.imageWithData(nsData) ?: return ByteArray(width * height * 3)
         
-        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
-        uiImage.drawInRect(CGRectMake(0.0, 0.0, width.toDouble(), height.toDouble()))
-        val resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+        val colorSpace = CGColorSpaceCreateDeviceRGB()
+        val bytesPerPixel = 4
+        val bytesPerRow = width * bytesPerPixel
+        
+        val context = CGBitmapContextCreate(
+            data = null,
+            width = width.toULong(),
+            height = height.toULong(),
+            bitsPerComponent = 8u,
+            bytesPerRow = bytesPerRow.toULong(),
+            space = colorSpace,
+            bitmapInfo = CGImageAlphaInfo.kCGImageAlphaNoneSkipLast.value
+        ) ?: return ByteArray(width * height * 3)
 
-        // Placeholder for iOS byte extraction
-        return ByteArray(width * height * 3)
+        UIGraphicsPushContext(context)
+        uiImage.drawInRect(CGRectMake(0.0, 0.0, width.toDouble(), height.toDouble()))
+        UIGraphicsPopContext()
+
+        val rawData = CGBitmapContextGetData(context) ?: run {
+            CGContextRelease(context)
+            CGColorSpaceRelease(colorSpace)
+            return ByteArray(width * height * 3)
+        }
+        
+        val bytePtr = rawData.reinterpret<ByteVar>()
+        val rgbByteArray = ByteArray(width * height * 3)
+
+        for (i in 0 until width * height) {
+            rgbByteArray[i * 3 + 0] = bytePtr[i * 4 + 0] // R
+            rgbByteArray[i * 3 + 1] = bytePtr[i * 4 + 1] // G
+            rgbByteArray[i * 3 + 2] = bytePtr[i * 4 + 2] // B
+        }
+
+        CGContextRelease(context)
+        CGColorSpaceRelease(colorSpace)
+
+        return rgbByteArray
     }
 
     @OptIn(ExperimentalForeignApi::class)
     private fun ByteArray.toNSData(): NSData = usePinned {
-        NSData.dataWithBytes(it.addressOf(0), it.size.toULong())
+        NSData.dataWithBytes(it.addressOf(0), this.size.toULong())
     }
 }
